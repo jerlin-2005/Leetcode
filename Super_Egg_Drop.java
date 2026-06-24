4ff876b430604d270b59c053fead2225b644ebec
Jerlin <jerlinjames2005_bcs27@mepcoeng.ac.in>
2026-06-24 11:28:59 +0530

    Implement superEggDrop function for egg drop problem

---
+++ Super_Egg_Drop.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int superEggDrop(int k, int n) {
+        int[] dp=new int[k+1];
+        int moves=0;
+        while(dp[k]<n){
+            moves ++;
+            for(int i=k;i>0;i--){
+                dp[i]=dp[i-1]+dp[i]+1;
+            }
+
+        }
+        return moves;
+    }
+}
